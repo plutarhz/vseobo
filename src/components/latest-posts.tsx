913c298ad1bebccea8a9dbc92dd75ef6eee27224
maksi <maksi@million>
2025-06-13 17:27:53 +0600

    article css

--- src/components/latest-posts.tsx
+++ src/components/latest-posts.tsx
@@ -41,7 +41,7 @@ export function LatestPosts({ title, posts, searchTerm, pageInfo, category, curr
   }, [])
 
   if (posts?.length === 0) {
-    return <div className="article">Нет записей</div>
+    return <div className="bg-[#fdf6ec]">Нет записей</div>
   }
 
   return (
