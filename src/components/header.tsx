c577ec5d99f2d3f4aa3ecfb6ba19b0792828433d
maksi <maksi@million>
2025-06-26 21:25:20 +0600

    header link img

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -12,41 +12,41 @@ export function Header() {
   return (
     <header className="rounded-sm py-2 mb:py-4 px-4 md:px-8 mb-4">
 
-
+      {/* {pathname !== '/' && ( здесь можно вставить то что показывать или нет )} */}
       <nav>
         <ul className='flex justify-around items-center gap-4'>
-          {pathname !== '/' && (
-            <li className="relative">
-              <Link href="/" className="absolute t-0 l-0 w-full h-full" aria-label="Перейти на главную страницу"></Link>
-              <Image
-                src="/home-2-svgrepo-com-white.svg"
-                width={40}
-                height={40}
-                quality={90}
-                placeholder="blur"
-                blurDataURL="/home-2-svgrepo-com-white.svg"
-                loading="eager" alt="На главную"
-              />
-            </li>
-          )}
+
+          <li className="relative">
+            <Link href="/" className="absolute t-0 l-0 w-full h-full" aria-label="Перейти на главную страницу"></Link>
+            <Image
+              src="/home-2-svgrepo-com-white.svg"
+              width={40}
+              height={40}
+              quality={90}
+              placeholder="blur"
+              blurDataURL="/home-2-svgrepo-com-white.svg"
+              loading="eager" alt="На главную"
+            />
+          </li>
+
           <li className="relative">
             <Link href={'/'} className="absolute t-0 l-0 w-full h-full" aria-label="Перейти на главную страницу"></Link>
             <span className={`${literata.className} text-4xl text-white font-bold`}>Всё обо всём</span>
           </li>
-          {pathname !== '/blog' && (
-            <li className="relative">
-              <Link href={'/blog'} className="absolute t-0 l-0 w-full h-full" aria-label="Перейти в энциклопедию"></Link>
-              <Image
-                src="/book-svgrepo-com-white.svg"
-                width={40}
-                height={40}
-                quality={90}
-                placeholder="blur"
-                blurDataURL="/book-svgrepo-com-white.svg"
-                loading="eager" alt="Книга знаний - Всё обо всём"
-              />
-            </li>
-          )}
+
+          <li className="relative">
+            <Link href={'/blog'} className="absolute t-0 l-0 w-full h-full" aria-label="Перейти в энциклопедию"></Link>
+            <Image
+              src="/book-svgrepo-com-white.svg"
+              width={40}
+              height={40}
+              quality={90}
+              placeholder="blur"
+              blurDataURL="/book-svgrepo-com-white.svg"
+              loading="eager" alt="Книга знаний - Всё обо всём"
+            />
+          </li>
+
         </ul>
       </nav>
 
