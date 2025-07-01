f64bdfb99a8edccd2999acca8e6c180a00d0b5b7
maksi <maksi@million>
2025-07-02 00:58:42 +0600

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export function Footer(){
     return (
       <footer className="text-center rounded-b-sm bg-[#fdf6ec] p-6 md:p-8">
-        <p>&copy; 1993 Всё обо всём &middot; Познавательная литература</p>
+        <p>&copy; 1993 Всё обо всём</p>
       </footer>
     )
   }
