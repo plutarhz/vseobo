43126e47c6f5747ad8add1a1704d55bd01c653c9
maksi <maksi@million>
2025-07-02 14:41:06 +0600

    header btns

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -132,7 +132,7 @@ export default async function Page({ params }: Props) {
 
       {/* Безопасное использование dangerouslySetInnerHTML */}
       <div
-        className="article md:text-lg text-base text-justify font-medium md:leading-relaxed text-[#3a3127] antialiased"
+        className="article text-base md:text-lg text-justify font-medium md:leading-relaxed text-[#3a3127] antialiased"
         dangerouslySetInnerHTML={{ __html: content }}
       />
       {/* Блок "Читать ещё" */}
