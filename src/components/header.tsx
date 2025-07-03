213384a8299ec63e8c7a103d66d7ed7aeca75bfb
maksi <maksi@million>
2025-07-04 01:42:44 +0600

    post

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,7 +18,7 @@ export function Header() {
 
           <li className="relative">
             <Link href={'/'} className="absolute t-0 l-0 w-full h-full" aria-label="Перейти на главную страницу"></Link>
-            <span className={`${literata.className} text-3xl md:text-4xl text-center text-white font-bold text-shadow-sm`}>Всё обо всём</span>
+            <span className={`${literata.className} text-3xl md:text-4xl text-center text-white font-bold text-shadow-white`}>Всё обо всём</span>
           </li>
 
           {/* <li className="relative">
