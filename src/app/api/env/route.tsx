2d0a91c69450e59cc07c05fd82375e6941a5bb38
maksi <maksi@million>
2025-07-03 15:43:29 +0600

    sitemap issu

--- src/app/api/env/route.tsx
+++ src/app/api/env/route.tsx
@@ -1,6 +1,16 @@
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  const env = process.env.NEXT_PUBLIC_SITE_URL || 'Не задан'
-  return NextResponse.json({ NEXT_PUBLIC_SITE_URL: env })
+  const envVars = {
+    NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+    SITE_URL_FALLBACK: process.env.SITE_URL_FALLBACK,
+    VERCEL_GIT_COMMIT_REF: process.env.VERCEL_GIT_COMMIT_REF,
+    NODE_ENV: process.env.NODE_ENV,
+    HOST: process.env.HOST,
+    VERCEL: process.env.VERCEL,
+    VERCEL_URL: process.env.VERCEL_URL,
+    LOCALHOST_FALLBACK: 'https://vsbvsm.ru ',
+  }
+
+  return NextResponse.json(envVars)
 }
