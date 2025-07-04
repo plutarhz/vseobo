0b895f9d71ca737d109ea61cab59592a4add0ada
maksi <maksi@million>
2025-07-05 05:18:43 +0600

    categories

--- src/components/categories.tsx
+++ src/components/categories.tsx
@@ -40,8 +40,8 @@ export function Categories({ categories }: { categories: Category[] }) {
     <ul className="text-sm uppercase flex gap-2 flex-wrap">
       {sortedCategories.map((category) => (
         <li key={category.id} className="flex flex-shrink-0 border rounded-md relative group">
-          <Link href={`/category/${category.slug}`} className="absolute top-0 left-0 right-0 bottom-0 z-10" />
-          <span className="px-2 py-1 relative z-20">{category.name}</span>
+          <Link href={`/category/${category.slug}`} className="absolute top-0 left-0 right-0 bottom-0 z-20" />
+          <span className="px-2 py-1 relative z-10">{category.name}</span>
           <span className="absolute bottom-0 left-0 w-0 h-0.5 bg-[#171717] group-hover:w-full group-hover:transition-all" />
           {typeof category.count === 'number' && (
             <span className="px-2 py-1 border-l-1 font-bold">
