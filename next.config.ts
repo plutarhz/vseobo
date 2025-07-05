c7a598c9c3f823a28b536d23e0c8d7ef5c61bfa5
maksi <maksi@million>
2025-07-05 15:42:46 +0600

    redirect

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,33 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   swcMinify: true,
   images: {
-    domains: ["194.87.99.150", "https://vsbvsm.ru", "https://vsbvsm.online", "https://vseobovsemm.duckdns.org"], // Добавь сюда все нужные домены/IP
+    domains: ["194.87.99.150", "vsbvsm.ru", "vsbvsm.online", "vseobovsemm.duckdns.org"],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.vsbvsm.ru", // заменить на свой www-домен
+          },
+        ],
+        permanent: true,
+        destination: "https://vsbvsm.ru/ :path*", // без www
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.vsbvsm.online",
+          },
+        ],
+        permanent: true,
+        destination: "https://vsbvsm.online/ :path*",
+      },
+    ];
   },
 };
 
