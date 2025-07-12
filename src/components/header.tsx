ae560f8a2bfa2382691b0ed9334d053d1ac66596
maksi <maksi@million>
2025-07-13 02:55:02 +0600

    goal

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -38,7 +38,7 @@ export function Header() {
           <li className="relative">
             <Link
               href="/blog"
-              className="absolute t-0 l-0 w-full h-full z-18"
+              className="absolute t-0 l-0 size-45px z-18"
               aria-label="Перейти в энциклопедию"
               onClick={() => {
                 console.log('Цель "blog" вызвана');
