33149bee7b5038d6179bc83426854e918943fdb4
maksi <maksi@million>
2025-07-12 18:41:44 +0600

    og main page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import "./globals.css";
-import type { Metadata } from "next";
 import { opensans } from '@/app/fonts';
 import { Header } from "@/components/header";
 import { Footer } from "@/components/footer";
@@ -10,32 +9,7 @@ import { ProgressBarProvider } from './providers';
 import Script from 'next/script'
 
 
-export const metadata: Metadata = {
-  title: "Всё обо всём - энциклопедия",
-  description: "Книга подготовлена при участии Центра гуманитарных наук при факультете журналистики МГУ им. М. В. Ломоносова",
-  openGraph: {
-    title: "Всё обо всём - энциклопедия",
-    description: "Книга подготовлена при участии Центра гуманитарных наук при факультете журналистики МГУ им. М. В. Ломоносова",
-    url: "https://vsbvsm.ru",  // ← замени на свой домен
-    siteName: "Всё обо всём",
-    images: [
-      {
-        url: "/catty.jpg", // ← можно сгенерировать автоматически или указать статичное из public/
-        width: 800,
-        height: 600,
-        alt: "Обложка энциклопедии «Всё обо всём»",
-      },
-    ],
-    locale: "ru_RU",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Всё обо всём - энциклопедия",
-    description: "Книга подготовлена при участии Центра гуманитарных наук при факультете журналистики МГУ им. М. В. Ломоносова",
-    images: ["/catty.jpg"], // ← ссылка на изображение
-  },
-};
+
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,37 @@
 import { Hero } from '@/components/hero';
+import type { Metadata } from "next";
 // import { SocialIcons } from '@/components/social-icons';
 import { Categories } from '@/components/categories';
 import { LatestPosts } from '@/components/latest-posts';
 import { getCategories, getAllPosts } from '@/lib/queries';
 import Link from 'next/link';
+
+export const metadata: Metadata = {
+  title: "Всё обо всём - энциклопедия",
+  description: "Книга подготовлена при участии Центра гуманитарных наук при факультете журналистики МГУ им. М. В. Ломоносова",
+  openGraph: {
+    title: "Всё обо всём - энциклопедия",
+    description: "Книга подготовлена при участии Центра гуманитарных наук при факультете журналистики МГУ им. М. В. Ломоносова",
+    url: "https://vsbvsm.ru",  // ← замени на свой домен
+    siteName: "Всё обо всём",
+    images: [
+      {
+        url: "/catty.jpg", // ← можно сгенерировать автоматически или указать статичное из public/
+        width: 800,
+        height: 600,
+        alt: "Обложка энциклопедии «Всё обо всём»",
+      },
+    ],
+    locale: "ru_RU",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Всё обо всём - энциклопедия",
+    description: "Книга подготовлена при участии Центра гуманитарных наук при факультете журналистики МГУ им. М. В. Ломоносова",
+    images: ["/catty.jpg"], // ← ссылка на изображение
+  },
+};
 
 export default async function Home() {
   const categories = await getCategories();
