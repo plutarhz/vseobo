24b194f6ab9abf039072c253101554b5ad505f51
maksi <maksi@million>
2025-07-12 19:01:56 +0600

    og blog page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,33 @@
 import { LatestPosts } from '@/components/latest-posts';
 import { getAllPosts } from '@/lib/queries';
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Страница всех записей энциклопедии Все обо всем",
+  description: "Здесь вы найдете все записи этой увлекательной энциклопедии",
+  openGraph: {
+    title: "Страница всех записей энциклопедии Все обо всем",
+    description: "Здесь вы найдете все записи этой увлекательной энциклопедии",
+    url: "https://vsbvsm.ru/blog",  // ← замени на свой домен
+    siteName: "Всё обо всём",
+    images: [
+      {
+        url: "/catty.jpg", // ← можно сгенерировать автоматически или указать статичное из public/
+        width: 800,
+        height: 600,
+        alt: "Обложка энциклопедии «Всё обо всём»",
+      },
+    ],
+    locale: "ru_RU",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Всё обо всём - энциклопедия",
+    description: "Книга подготовлена при участии Центра гуманитарных наук при факультете журналистики МГУ им. М. В. Ломоносова",
+    images: ["/catty.jpg"], // ← ссылка на изображение
+  },
+};
 
 type Params = Promise<{ slug: string }>
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>
